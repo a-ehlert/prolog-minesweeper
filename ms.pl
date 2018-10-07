2c4d331c309ab3be30d0c95c4a16dc2ddb27e453
Anton Ehlert <anton.ehlert@gmail.com>
2018-10-07 08:38:25 +0200

    2nd commit

--- ms.pl
+++ ms.pl
@@ -1,20 +1,69 @@
 :- use_module(library(lists)), use_module(library(random)), use_module(library(ordsets)).
 
 %Cells is a list of rows of cells
+%Valid moves: Reveal,flag,
 
 play(Game) :-
-	initialize(Game,State),
-	display_game(State).
-	%play(State,Result).
+	%initialize(Game,State),
+	State = (10,10,15,_Cells,_Mines),
+	initialize(Game,State), 
+	display_game(State),
+	play(State,_Result).
 
-initialize(minesweeper,(Height,Width,Mines,Cells)) :-
+%play(State,Result) :-
+%	game_over(State,Result),
+%	!, announce(Result).
+
+play(State,Result) :-
+	choose_move(State,Move),
+	legal_move(State,Move),
+	write('legal move!'), nl,
+	move(State,Move),
+	display_game(State),
+	!, play(State,Result).
+
+display_game((_Height,Width,_NMines,Cells,_Mines)) :-
+	display_cells(Cells,Width,0),
+	nl.
+
+
+choose_move(State,Move) :-
+	player_move(State,Move).
+choose_move(State,Move) :-
+	write('ai move.'), nl,
+	ai_move(State,Move).
+choose_move(_,_) :-
+	write('No move.'), nl.
+
+player_move(_State,(Y,X)) :-
+	write('enter move:'),
+	nl,
+	catch(read((Y,X)), _Error, false).
+
+ai_move(_State,(_Y,_X)) :-
+	false.
+
+legal_move((Height,Width,_,_,_),(Y,X)) :-
+	\+((Y<0; X<0; Height=<Y; Width=<X)).
+
+
+move((_Height,_Width,_NMines,Cells,Mines),Move) :-
+	write('Time to move...'), nl,
+	reveal(Move,Cells,Mines).
+
+%prompt(X) :-
+	
+
+game_over(_State,_Result) :-
+	fail.
+
+initialize(minesweeper,(Height,Width,NMines,Cells,Mines)) :-
 	Height>0, Width>0,
-	assert((invalidcoord(Y,X):- Y<0; X<0; Height=<Y; Width=<X)),
+	%assert((invalidcoord(Y,X):- Y<0; X<0; Height=<Y; Width=<X)),
 	NCells is Height*Width,
-	Mines =< NCells,
+	NMines =< NCells,
 	build_cells(0,0,Height,Width,Cells),
-	place_mines(Cells,Mines),
-	display_cells(Cells,Width,0).
+	place_mines(NMines,Cells,Mines).
 
 build_cells(Y,X,H,W,[cell(Y,X,_)|Cells]) :-
 	Y < H,
@@ -24,32 +73,55 @@ build_cells(Y,X,H,W,[cell(Y,X,_)|Cells]) :-
 build_cells(H,_,H,_,[]).
 
 
-place_mines(Cells,M) :-
-	M > 0,
-	random_select(C,Cells,Cells1),
-	assert(mine(C)),
-	C=cell(_,_,*),
+place_mines(M,Cells,[Mine|Mines]) :-
+	M > 0,!,
+	random_select(Mine,Cells,Cells1),
+	Mine = cell(_,_,*),
 	M1 is M-1,
-	place_mines(Cells1,M1).
-place_mines(_,0).
+	place_mines(M1,Cells1,Mines).
+place_mines(0,_,[]).
 
-reveal(cell(Y,X,Content),Cells) :-
-	memberchk(cell(Y,X,Content),Cells).
+%If no adjacent, reveal neighbours
+reveal((Y,X),Cells,Mines) :-
+	%write('reveal 1 called'),nl,
+	n_adjacent(cell(Y,X,_),Mines,0),!,
+	memberchk(cell(Y,X,' '),Cells),
+	all_adjacent_unrevealed(cell(Y,X,_),Cells,Adj),
+	fill_reveal(Adj,Cells,Mines).
+reveal((Y,X),Cells,Mines) :-
+	%write('reveal 2 called'),nl,
+	n_adjacent(cell(Y,X,_),Mines,N),
+	memberchk(cell(Y,X,N),Cells).
+
+fill_reveal([cell(Y,X,_)|Cs],Cells,Mines) :-
+	reveal((Y,X),Cells,Mines),
+	fill_reveal(Cs,Cells,Mines).
+fill_reveal([],_,_).
+
+unrevealed(cell(_Y,_X,Content)) :-
+	var(Content).
+
+%move((Y,X),(Cells,Mines)) :-
+	
+
+
+	
+
 
 
 %move(cell()).
 %cell(X,Y,_) :- \+ invalidcoord(X,Y).
 
 
-left_right(cell(X1,_,_),cell(X2,_,_)) :-
-	integer(X1), !, X2 is X1+1.
-left_right(cell(X1,_,_),cell(X2,_,_)) :-
-	integer(X2), !, X1 is X2-1.
-
-up_down(cell(_,Y1,_),cell(_,Y2,_)) :-
+up_down(cell(Y1,_,_),cell(Y2,_,_)) :-
 	integer(Y1), !, Y2 is Y1+1.
-up_down(cell(_,Y1,_),cell(_,Y2,_)) :-
+up_down(cell(Y1,_,_),cell(Y2,_,_)) :-
 	integer(Y2), !, Y1 is Y2-1.
+
+left_right(cell(_,X1,_),cell(_,X2,_)) :-
+	integer(X1), !, X2 is X1+1.
+left_right(cell(_,X1,_),cell(_,X2,_)) :-
+	integer(X2), !, X1 is X2-1.
 
 left_or_right(A,B) :- left_right(A,B).
 left_or_right(A,B) :- left_right(B,A).
@@ -76,21 +148,32 @@ adjacent(A,B) :-
 adjacent(A,B) :-
 	diagonal(A,B).
 
+coords(cell(Y,X,_),Y,X).
+
 %Adj is a list of all adjacent cells to Cell in Cells.
 all_adjacent(Cell,Cells,Adj) :-
 	findall(C,(adjacent(Cell,C),memberchk(C,Cells)),Adj).
 
+all_adjacent_unrevealed(Cell,Cells,Adj) :-
+	findall(C,(adjacent(Cell,C),memberchk(C,Cells),unrevealed(C)),Adj).
+
+n_adjacent(Cell,Cells,N) :-
+	all_adjacent(Cell,Cells,Adj), length(Adj,N).
+
 display_cells([Cell|Cells],Width,N) :-
 	N < Width, N1 is N+1,
 	display_cell(Cell), display_cells(Cells,Width,N1).
-display_cells(Cells,Width,Width) :-
-	format("~n",[]),
-	display_cells(Cells,Width,0).
+display_cells([Cell|Cells],Width,Width) :-
+	nl,
+	display_cells([Cell|Cells],Width,0).
 display_cells([],_,_).
 
-display_cell(cell(X,Y,Content)) :-
+display_cell(cell(_Y,_X,Content)) :-
 	var(Content),
 	format("-",[]).
-display_cell(cell(X,Y,Content)) :-
+display_cell(cell(_Y,_X,Content)) :-
+	integer(Content),
+	format("~d",[Content]).
+display_cell(cell(_Y,_X,Content)) :-
 	nonvar(Content),
 	format(Content,[]).
